b3ed93ce82fbd77294e32dc449d53c6104ec5843
Shawn <shawntaylor@weecology.org>
2016-10-27 11:24:08 -0400

    much simpler method for this

--- convert_prism_to_bioclim.R
+++ convert_prism_to_bioclim.R
@@ -2,6 +2,8 @@ library(prism)
 library(raster)
 library(magrittr)
 
+options(prism.path = "~/data/prismdata")
+
 ########################################################
 #Download prism data if not already there
 ###
@@ -32,38 +34,52 @@ max_min_combo=function(vec1,vec2,max=TRUE){
 #Function used in raster::overlay() which accepts
 create_bioclim_stack = function(prism_year_stacked){
   
-  #Get location of the different variables for each month
-  stack_info=data.frame()
-  for(layer in 1:length(prism_year_stacked@layers)){
-    layer_name = prism_year_stacked[[layer]]@data@names
-    layer_var  = stringr::word(layer_name, 2,2, '_')
-    layer_month= stringr::word(layer_name, 5,5, '_')
-    layer_month= as.integer(stringr::str_sub(layer_month, 5, 6))
-    
-    stack_info = stack_info %>%
-      bind_rows(data.frame(layer=layer, var=layer_var, month=layer_month))
-  }
-   
+  df=as.data.frame(prism_year_stacked) 
+  df$cell_id = 1:nrow(df)
+  df = df %>%
+    tidyr::gather(layer_name, value, -cell_id) %>%
+    tidyr::extract(layer_name, c("clim_var", "year", "month"),
+                   "PRISM_([:alpha:]*)_stable_[:alnum:]*_([:digit:]{4})([:digit:]{2})_",
+                   convert=TRUE) %>%
+    tidyr::spread(clim_var, value)
+
+  #Process the quarter ones first.
+  quarter_info=data.frame(month=1:12, quarter=c(1,1,1,2,2,2,3,3,3,4,4,4))
+  bioclim_quarter_data= df %>%
+    dplyr::left_join(quarter_info, by='month') %>%
+    dplyr::group_by(cell_id, quarter) %>%
+    dplyr::summarize(precip=sum(ppt), temp=mean(tmean)) %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(cell_id) %>%
+    dplyr::summarize(bio8=max_min_combo(temp, precip, max=TRUE),
+              bio9=max_min_combo(temp, precip, max=FALSE),
+              bio10=max(temp),
+              bio11=min(temp),
+              bio16=max(precip),
+              bio17=min(precip),
+              bio18=max_min_combo(precip, temp, max=TRUE),
+              bio19=max_min_combo(precip, temp, max=FALSE)) %>%
+    dplyr::ungroup()  
+ 
+  #Next the yearly ones, joining the quartely ones  back in at the end. 
+  bioclim_data=df %>%
+    dplyr::group_by(cell_id) %>%
+    dplyr::mutate(monthly_temp_diff=tmax-tmin) %>%
+    dplyr::summarize(bio1=mean(tmean),
+              bio2=mean(monthly_temp_diff),
+              bio4=sd(tmean)*100,
+              bio5=max_min_combo(tmax,tmean,max=TRUE),
+              bio6=max_min_combo(tmin,tmean,max=FALSE),
+              bio12=sum(ppt),
+              bio13=max(ppt),
+              bio14=min(ppt),
+              bio15=cv(ppt)) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(bio7=bio5-bio6,
+                  bio3=(bio2/bio7)*100) %>%
+    dplyr::full_join(bioclim_quarter_data, by=c('cell_id'))
   
-  y
-  
-  #Assign each month to a quarter 
-  stack_info = stack_info %>%
-    left_join(data.frame(month=1:12, quarter=c(1,1,1,2,2,2,3,3,3,4,4,4)), by='month')
-  
-  bio1_layers = stack_info %>%
-    filter(var == 'tmean') %>%
-    extract2('layer')
-  
-  bio1_stack = prism_year_stacked[[bio1_layers]]
-  
-  bio1_raster = overlay(bio1_stack, fun=mean)
-  
-  
-  bio2_layers = stack_info %>%
-    
-  
-    
+     
 }
 
 
