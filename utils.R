252eef55d892eb98b6221ce1606c9c83f3f425af
Shawn <shawntaylor@weecology.org>
2017-01-27 15:47:48 -0500

    functions for getting spatial data during analysis

---
+++ utils.R
@@ -0,0 +1,27 @@
+source('config.R')
+
+
+####################################################################
+#Load the bioclim variables from sqlite.
+get_bioclim_data=function(){
+  #Query sqlite database for the bioclim_bbs_data table. If it exists, return it.
+  #Otherwise create it from the raw prism data.
+  if('bioclim_bbs_data' %in% src_tbls(database)){
+    return(collect(tbl(database, sql('SELECT * from bioclim_bbs_data')), n=Inf))
+  } else { 
+    print("bioclim data table not found. need to run load_scaled_bioclim.R")
+  } 
+}
+
+####################################################################
+#Load the data frame that tells which site is in which cell at all the 
+#different spatial scales
+get_spatial_grid_info=function(){
+  #Query sqlite database for thetable. If it exists, return it.
+  #Otherwise throw an error
+  if('spatial_grid_info' %in% src_tbls(database)){
+    return(collect(tbl(database, sql('SELECT * from spatial_grid_info')), n=Inf))
+  } else { 
+    stop("spatial_grid_info not found, need to run load_scaled_bioclim.R")
+  } 
+}
