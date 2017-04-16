8e4b956ba22adb1017dfa9050bfe9ad10d92d141
Shawn <shawntaylor@weecology.org>
2017-04-16 11:30:58 -0400

    clarify spatial scales used

--- config.R
+++ config.R
@@ -11,15 +11,11 @@ testing_years=2000:2014
 #Grain sizes in time and space
 temporal_scales=c(1,3,5,10)
 
-#Spatial scale to use. 
-#in Deg lat/long
-spatial_scales=c(0.1, 1.0, 2.0, 4.0)
-#spatial_scales=c(0.1, 0.25, 0.5, 1.0, 2.0)
-#in km
-#spatial_scales=c(11,  28,   56,  111, 212)
-#in number of 4km grid cells
-#spatial_scales=c(3,    7,    14,  28,  53)
-
+#Spatial scales to use. 
+#in km on a side. 
+#spatial_scales=c(40, 80, 160, 320)
+#in Deg lat/long. Calculated as km*0.01 (very rough approximiation but suitable for this analysis)
+spatial_scales=c(0.4, 0.8, 1.6, 3.2)
 
 # Formula to pass to models
 modelFormula=as.formula('presence ~ bio1+bio2+bio4+bio5+bio6+bio7+bio8+bio9+bio10+bio11+bio12+bio13+bio14+bio16+bio17+bio18+bio19')
