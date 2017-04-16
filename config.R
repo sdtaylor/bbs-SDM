ca60fb2fecbd2f1e301d922b3f9877c47d1ec265
Shawn <shawntaylor@weecology.org>
2017-04-16 11:59:07 -0400

    update scales to match method 1

--- config.R
+++ config.R
@@ -10,8 +10,11 @@ temporal_scales=c(1,3,5,10)
 training_years=1971:1980
 testing_years=2000:2014
 
-#Spatial scale to use. These are width & height of grid cells in degrees. 
-spatial_scales=c(0.1, 0.25, 0.5, 1.0, 2.0)
+#Spatial scales to use. 
+#in km on a side. 
+#spatial_scales=c(40, 80, 160, 320)
+#in Deg lat/long. Calculated as km*0.01 (very rough approximiation but suitable for this analysis)
+spatial_scales=c(0.4, 0.8, 1.6, 3.2)
 
 # Formula to pass to models
 modelFormula=as.formula('presence ~ bio1+bio2+bio4+bio5+bio6+bio7+bio8+bio9+bio10+bio11+bio12+bio13+bio14+bio16+bio17+bio18+bio19')
